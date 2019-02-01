56573a68fba05ffb0d073073bd5b78e44fb98a72
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-02-01 15:10:43 -0800

    add price prototype, to pull info from pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,47 @@
-// function Pizza(size, toppings){
-//   this.size = size;
-//   this.toppings = toppings;
-// }
+function Pizza(size, toppings){
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.price = function() {
+return this.size + this.toppings.length*2.50;
+}
+
+
 
 
 $(document).ready(function() {
+
   var toppings = [];
+  var sizePrice = "";
 
   $("#pizzaCreator").submit(function(event) {
   var size = $("input:radio[name=size]:checked").val();
+
+  if (size === "personal") {
+    sizePrice = 7;
+  }
+  else if (size == "medium") {
+    sizePrice=11;
+  }
+  else if (size==="large") {
+    sizePrice=14;
+  }
+  else {
+    sizePrice=16;
+  }
    $("input:checkbox[name=toppings]:checked").each(function(){
       var toppingChoice = $(this).val();
-      toppings.push(toppingChoice)
+      toppings.push(toppings)
     })
-    console.log(size, toppings);
+
+
+    var pizza = new Pizza(sizePrice, toppings);
+
+
+
+    console.log(pizza);
+    console.log("Your total comes to: $" + pizza.price());
     event.preventDefault();
   })
 });
